976a364a6040af70e2638ab12953b88f7c3eaead
JessePinkman <affangashanta@gmail.com>
2026-04-01 09:06:25 +0700

    membuat class induk user dan class turunan

--- LatihanHeritence/Program.cs
+++ LatihanHeritence/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Siswa siswaobj = new Siswa("Thariq", "Thariq@gmail.com", "12345", "siswa", "XPG1");
+Guru guruobj = new Guru("Amie", "Amir@gmail.com", "678910", "guru", "Matematika");
+
+siswaobj.TampilkanInfo();
+siswaobj.ScubmitTugas();
+
+Console.WriteLine();
+
+guruobj.TampilkanInfo();
+guruobj.NilaiTugas();
+public class User
+{
+    public string nama;
+    public string email;
+    public string password;
+    public string role;
+
+    public User(string nama, string email, string password, string role)
+    {
+        this.nama = nama;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public void TampilkanInfo()
+    {
+        Console.WriteLine($"Nama: {nama} | Role: {role}");
+    }
+}
+class Siswa : User
+{
+    public string className;
+
+    public Siswa(string nama, string email, string password, string role, string className) : base(nama, email, password, role)
+    {
+        className = className;
+    }
+
+    public void ScubmitTugas()
+    {
+        Console.WriteLine($"{nama} dari kelas {className} sudah mengumpulkan tugas");
+    }
+
+}
+
+class Guru : User
+{
+    public string mapel;
+
+    public Guru(string nama, string email, string password, string role, string mapel) : base(nama, email, password, role)
+    {
+        mapel = mapel;
+    }
+
+    public void NilaiTugas()
+    {
+        Console.WriteLine($"Guru {nama} telah manilai semua tugas {mapel}");
+    }
+}
